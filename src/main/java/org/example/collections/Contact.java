9ea6bf46d6b32eaaa5d43de73445aa8169834c88
mar.pilip <mar.pilip@gmail.com>
2025-02-23 22:42:32 +0700

    Домашнее задание: Проект. Модуль Collections

---
+++ src/main/java/org/example/collections/Contact.java
@@ -0,0 +1,46 @@
+package org.example.collections;
+
+import java.util.Objects;
+
+public class Contact {
+    String name;
+    String phone;
+    String email;
+    String group;
+
+    public Contact(String name, String phone, String email, String group) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.group = group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(name, contact.name) && Objects.equals(phone, contact.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "Контакт: " + name +
+                "\n Номер телефона: " + phone +
+                "\n email: " + email +
+                "\n группа: " + group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+}
---
+++ src/main/java/org/example/collections/exceptions/ContactNotFoundException.java
@@ -0,0 +1,7 @@
+package org.example.collections.exceptions;
+
+public class ContactNotFoundException extends Exception {
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+}
